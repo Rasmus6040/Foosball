7453f3e41e25073f16cdebd0d130644101566398
Joachim Horshauge <joachim.horshauge@boostplm.dk>
2024-11-09 15:02:39 +0100

    Dynamic match creation in frontend

--- FrontEnd/Components/MatchCard.razor.cs
+++ FrontEnd/Components/MatchCard.razor.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Components;
+using DTO.Data;
 
 namespace FrontEnd.Components;
 
 public partial class MatchCard : ComponentBase
 {
-    
-    
+    [Parameter] public Match Match { get; set; } = default!;
 }
